9d69034761631082465f83ee7152d435172e62bb
Faizan-Ramzan <36534453+Faizan-Ramzan@users.noreply.github.com>
2022-06-02 17:32:18 +0500

    Preview and word count

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -16,16 +16,27 @@ export default function TextForm(props) {
         setText(event.target.value);
     }
 
-    const [text, setText] = useState("Enter Text Here");
+    const [text, setText] = useState("");
 
     return (
-        <div className="container mt-5">
-            <h2 className="mb-4">{props.heading}</h2>
-            <div className="mb-3">
-                <textarea className="form-control" name="description" onChange={handleOnChange} value={text} cols="30" rows="10"></textarea>
+        <>
+            <div className="container mt-5">
+                <h2 className="mb-4">{props.heading}</h2>
+                <div className="mb-3">
+                    <textarea className="form-control" name="description" onChange={handleOnChange} value={text} cols="30" rows="10"></textarea>
+                </div>
+                <button type="submit" className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
+                <button type="submit" className="btn btn-danger mx-2" onClick={handleLoClick}>Convert to Lowercase</button>
             </div>
-            <button type="submit" className="btn btn-primary m-2" onClick={handleUpClick}>Convert to Uppercase</button>
-            <button type="submit" className="btn btn-danger" onClick={handleLoClick}>Convert to Lowercase</button>
-        </div>
+            <div className="container my-3">
+                <h1>Your Text Summary</h1>
+                <p>{text.split(" ").length} words and {text.length} characters</p>
+                <p>{0.008 * text.split(" ").length} Minutes Read</p>
+                <h3>Preview</h3>
+                <pre>
+                    {text}
+                </pre>
+            </div>
+        </>
     )
 }
