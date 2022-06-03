3d03611679d4196bff5e2d1c5e482f5cd7f81679
Faizan-Ramzan <36534453+Faizan-Ramzan@users.noreply.github.com>
2022-06-03 18:57:54 +0500

    New Changes 3-6-2022

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import './App.css';
-import About from './components/About';
+// import About from './components/About';
 import Navbar from './components/Navbar';
-// import TextForm from './components/TextForm';
+import TextForm from './components/TextForm';
+import React, {useState} from 'react'
 
 function App() {
+  
+  const [mode, setMode] = useState('light'); // dark mode enable or not 
+
+  const toggleMode = ()=>{
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = 'black';
+    }else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+    }
+  }
+
   return (
     <>
-      <Navbar title="Global" aboutText="About Global"/>
-      {/* <TextForm heading="Enter Text to analyze below"/> */}
-      <About />
+      <Navbar title="Global" aboutText="About Global" mode={mode} toggleMode={toggleMode} />
+      <TextForm heading="Enter Text to analyze below"/>
+      {/* <About /> */}
     </>
   );
 }
