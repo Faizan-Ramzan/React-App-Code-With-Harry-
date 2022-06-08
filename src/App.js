05540451b110d9d062080618f1b78c91a29b804c
Faizan-Ramzan <36534453+Faizan-Ramzan@users.noreply.github.com>
2022-06-08 18:17:02 +0500

    React Routers

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
-// import About from './components/About';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import About from './components/About';
 import Navbar from './components/Navbar';
+import NotFound from './components/NotFound';
 import TextForm from './components/TextForm';
 import React, {useState} from 'react'
 import Alert from './components/Alert';
@@ -19,21 +21,6 @@ function App() {
       setAlert(null);
     }, 1500);
   }
-
-  // const toggleMode = (colorName)=>{
-  //   if(mode === 'light'){
-  //     setMode('dark');
-  //     console.log(colorName.target.attributes.datacolor.nodeValue);
-  //     document.body.style.backgroundColor = '#272a2b';
-  //     document.body.style.color = 'white';
-  //     showAlert("Dark mode has been enabled", "success");
-  //   }else{
-  //     setMode('light');
-  //     document.body.style.backgroundColor = 'white';
-  //     document.body.style.color = '#272a2b';
-  //     showAlert("Light mode has been enabled", "success");
-  //   }
-  // }
 
   const toggleMode = (colorName)=>{
     if(colorName === 'light'){
@@ -56,10 +43,17 @@ function App() {
 
   return (
     <>
-      <Navbar title="Global" aboutText="About Global" mode={mode} toggleMode={toggleMode} />
-      <Alert alert={alert} />
-      <TextForm showAlert={showAlert} heading="Enter Text to analyze below Text" mode={mode} />
-      {/* <About /> */}
+      <BrowserRouter>
+        <Navbar title="Textutils" aboutText="About" mode={mode} toggleMode={toggleMode} />
+        <Alert alert={alert} />
+
+        <Routes>
+          <Route path="/" element={<TextForm showAlert={showAlert} heading="Enter Text to analyze below Text" mode={mode} />}/>
+          <Route path="/about" element={<About />}/>
+
+          <Route path="*" element={<NotFound />}/>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
---
+++ src/components/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function pageNotFound() {
+    return (
+        <>
+            <div class="container my-5">
+                <h1 class="alert alert-danger text-center">Page Not Found</h1>
+            </div>
+        </>
+    )
+}
+
+export default pageNotFound
